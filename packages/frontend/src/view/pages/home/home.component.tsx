8490cb4091d499a332b04aeeec7130e3c803f9f6
Anastasia Blinkova <nastya_blnk@mail.ru>
2024-03-06 10:34:54 +0300

    1 & 2

--- packages/frontend/src/view/pages/home/home.component.tsx
+++ packages/frontend/src/view/pages/home/home.component.tsx
@@ -3,6 +3,8 @@ import { FileList } from "@diploma/frontend/view/components/file-list/file-list.
 import { FC, useCallback, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
+import styles from "./home.module.scss";
+
 export const HomePage: FC = () => {
   const [files, setFiles] = useState<string[]>();
 
@@ -17,13 +19,31 @@ export const HomePage: FC = () => {
 
   return (
     <>
-      <ul>
+      {/* <ul>
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(id => (
           <li key={id}>
             <Link to={`/${id}`}>Что-то с ID={id}</Link>
           </li>
         ))}
-      </ul>
+      </ul> */}
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <th className={styles.th}>Имя</th>
+            <th className={styles.th}>Ссылка</th>
+          </tr>
+        </thead>
+        <tbody>
+          {files?.map(filename => (
+            <tr>
+              <td className={styles.td}></td>
+              <td className={styles.td}>
+                <Link to={`/${filename}`}>Что-то с ID={filename}</Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
       <FileList files={files ?? []} />
     </>
   );
