ca11936c8784b9e335cc406d44c6442ee16ed049
Anastasia Blinkova <nastya_blnk@mail.ru>
2024-04-23 13:27:37 +0300

    3

--- packages/frontend/src/view/pages/home/home.component.tsx
+++ packages/frontend/src/view/pages/home/home.component.tsx
@@ -1,5 +1,6 @@
 import { filesApi } from "@diploma/frontend/api/files.api";
-import { FileList } from "@diploma/frontend/view/components/file-list/file-list.component";
+
+// import { FileList } from "@diploma/frontend/view/components/file-list/file-list.component";
 import { FC, useCallback, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
@@ -19,32 +20,27 @@ export const HomePage: FC = () => {
 
   return (
     <>
-      {/* <ul>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(id => (
-          <li key={id}>
-            <Link to={`/${id}`}>Что-то с ID={id}</Link>
-          </li>
-        ))}
-      </ul> */}
       <table className={styles.table}>
         <thead>
           <tr>
-            <th className={styles.th}>Имя</th>
-            <th className={styles.th}>Ссылка</th>
+            <th>Имя</th>
+            <th>Ссылка</th>
           </tr>
         </thead>
         <tbody>
           {files?.map(filename => (
             <tr>
-              <td className={styles.td}></td>
-              <td className={styles.td}>
-                <Link to={`/${filename}`}>Что-то с ID={filename}</Link>
+              <td>
+                <p>{filename}</p>
+              </td>
+              <td>
+                <Link to={`/${filename}`}>Подробнее</Link>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
-      <FileList files={files ?? []} />
+      {/* <FileList files={files ?? []} /> */}
     </>
   );
 };
