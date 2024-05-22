1dbceff78a4e4875e2e0555de95597e16112763a
daniil.egorov <daniil.egorov@spbpu.com>
2024-05-22 23:50:06 +0300

    auth store

--- packages/frontend/src/view/layout/header.component.tsx
+++ packages/frontend/src/view/layout/header.component.tsx
@@ -1,12 +1,22 @@
 import { FC } from "react";
 
 import styles from "./layout.module.scss";
+import { useAuthStore } from "@diploma/frontend/store/auth.store";
 
 export const Header: FC = () => {
+  const { isLoggedIn, logout } = useAuthStore();
+
+  const handleLoginClick = () => {
+    // TODO: модальное окно с формой ввода логина и пароля
+    // Если логин и пароль совпадают с ожидаемыми, вызывается logout из useAuthStore
+  };
+
   return (
     <header className={styles.header}>
       <p>Программный модуль</p>
       <p>визуализации природных явлений на спутниковых снимках</p>
+      {/* TODO: поправить по верстке */}
+      {isLoggedIn ? <button onClick={() => logout()}>Выйти</button> : <button onClick={handleLoginClick}>Войти</button>}
     </header>
   );
 };
